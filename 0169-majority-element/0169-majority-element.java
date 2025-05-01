166142489901f9a2db35da340bdcf3bd9160ab09
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2025-05-01 12:20:04 +0530

    Time: 1 ms (99.87%), Space: 53.3 MB (38.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,35 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int candidate = 0;
+       int count = 0, candidate = -1;
 
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;  // Choose new candidate
-            }
-
-            // Vote: +1 for same, -1 for different
-            count += (num == candidate) ? 1 : -1;
-        }
-
-        return candidate;  // Assumes majority element always exists
+    // Finding majority candidate
+    for (int index = 0; index < nums.length; index++) {
+      if (count == 0) {
+        candidate = nums[index];
+        count = 1;
+      }
+      else {
+        if (nums[index] == candidate)
+          count++;
+        else
+          count--;
+      }
     }
-}
 
+    // Checking if majority candidate occurs more than
+    // n/2 times
+    count = 0;
+    for (int index = 0; index < nums.length; index++) {
+      if (nums[index] == candidate)
+        count++;
+    }
+    if (count > (nums.length / 2))
+      return candidate;
+    return -1;
+
+    // The last for loop and the if statement step can
+    // be skip if a majority element is confirmed to
+    // be present in an array just return candidate
+    // in that case
+  }
+}
