7117441921a4ecc6dafdb8755d8d58a030f2cab1
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2025-05-01 12:27:39 +0530

    Time: 16 ms (18.1%), Space: 49.3 MB (89.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,35 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int majorityElement(int[] nums) {
-       int count = 0, candidate = -1;
+        Map<Integer, Integer> map = new HashMap<>();
+        int majorityCount = nums.length / 2;
 
-    // Finding majority candidate
-    for (int index = 0; index < nums.length; index++) {
-      if (count == 0) {
-        candidate = nums[index];
-        count = 1;
-      }
-      else {
-        if (nums[index] == candidate)
-          count++;
-        else
-          count--;
-      }
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            if (map.get(num) > majorityCount) {
+                return num;
+            }
+        }
+
+        throw new IllegalArgumentException("No majority element found");
     }
-
-    // Checking if majority candidate occurs more than
-    // n/2 times
-    count = 0;
-    for (int index = 0; index < nums.length; index++) {
-      if (nums[index] == candidate)
-        count++;
-    }
-    if (count > (nums.length / 2))
-      return candidate;
-    return -1;
-
-    // The last for loop and the if statement step can
-    // be skip if a majority element is confirmed to
-    // be present in an array just return candidate
-    // in that case
-  }
 }
