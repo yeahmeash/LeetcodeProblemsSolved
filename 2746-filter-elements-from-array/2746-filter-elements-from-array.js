829db873538bd5207fb060971632b4b3d00af34a
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2025-05-12 03:54:25 +0530

    Time: 47 ms (25.02%), Space: 54.1 MB (16.51%) - LeetHub

---
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -0,0 +1,9 @@
+function filter(arr, fn) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
