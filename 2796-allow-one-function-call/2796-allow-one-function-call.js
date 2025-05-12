4f6b1ec55a65444db82a4f4067501712538d49f8
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2025-05-12 13:29:09 +0530

    Time: 34 ms (91.58%), Space: 53.6 MB (39.48%) - LeetHub

---
+++ 2796-allow-one-function-call/2796-allow-one-function-call.js
@@ -0,0 +1,15 @@
+function once(fn) {
+    let called = false;
+    let result;
+
+    return function(...args) {
+        if (!called) {
+            called = true;
+            result = fn(...args);
+            return result;
+        }
+        return undefined;
+    };
+}
+
+
