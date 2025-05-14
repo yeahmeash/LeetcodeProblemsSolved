e6d48290a6b08ad50426a687d4bdef24bebffde1
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2025-05-14 15:23:10 +0530

    Time: 1 ms (99.59%), Space: 57.2 MB (43.12%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // If currentSum is negative, start new subarray from nums[i]
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
