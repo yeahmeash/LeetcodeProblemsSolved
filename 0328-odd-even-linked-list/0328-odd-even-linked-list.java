e0ec6f47bc0cc1e666ac9317b559c25fa1dcbab1
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2025-08-29 08:53:33 +0530

    Time: 0 ms (100%), Space: 44.7 MB (37.82%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,21 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-  public ListNode oddEvenList(ListNode head) {
-    ListNode oddHead = new ListNode(0);
-    ListNode evenHead = new ListNode(0);
-    ListNode odd = oddHead;
-    ListNode even = evenHead;
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
 
-    for (boolean isOdd = true; head != null; head = head.next, isOdd = !isOdd)
-      if (isOdd) {
-        odd.next = head;
-        odd = odd.next;
-      } else {
-        even.next = head;
-        even = even.next;
-      }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
 
-    odd.next = evenHead.next;
-    even.next = null;
-    return oddHead.next;
-  }
+        while (even != null && even.next != null) {
+            odd.next = even.next;   // connect odd to next odd
+            odd = odd.next;
+
+            even.next = odd.next;   // connect even to next even
+            even = even.next;
+        }
+
+        odd.next = evenHead; // connect odd list with even list
+        return head;
+    }
 }
