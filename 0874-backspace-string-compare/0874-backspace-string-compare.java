e03f815e765ddac7c6b5b8c846feddd70428d99a
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2025-09-02 13:24:27 +0530

    Time: 0 ms (100%), Space: 41.6 MB (69.22%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.java
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -1,30 +1,32 @@
 class Solution {
-  public boolean backspaceCompare(String s, String t) {
-    int i = s.length() - 1; // s' index
-    int j = t.length() - 1; // t's index
+ public boolean backspaceCompare(String s, String t) {
+    int i = s.length() - 1, j = t.length() - 1;
+    int skipS = 0, skipT = 0;
 
-    while (true) {
-      // Delete characters of s if needed.
-      int backspace = 0;
-      while (i >= 0 && (s.charAt(i) == '#' || backspace > 0)) {
-        backspace += s.charAt(i) == '#' ? 1 : -1;
-        --i;
-      }
-      // Delete characters of t if needed.
-      backspace = 0;
-      while (j >= 0 && (t.charAt(j) == '#' || backspace > 0)) {
-        backspace += t.charAt(j) == '#' ? 1 : -1;
-        --j;
-      }
-      if (i >= 0 && j >= 0 && s.charAt(i) == t.charAt(j)) {
-        --i;
-        --j;
-      } else {
-        break;
-      }
+    while (i >= 0 || j >= 0) {
+        // Process s
+        while (i >= 0) {
+            if (s.charAt(i) == '#') { skipS++; i--; }
+            else if (skipS > 0) { skipS--; i--; }
+            else break;
+        }
+
+        // Process t
+        while (j >= 0) {
+            if (t.charAt(j) == '#') { skipT++; j--; }
+            else if (skipT > 0) { skipT--; j--; }
+            else break;
+        }
+
+        // Compare current valid chars
+        if (i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j)) return false;
+        if ((i >= 0) != (j >= 0)) return false; // one exhausted before other
+
+        i--; j--;
     }
 
-    return i == -1 && j == -1;
-  }
+    return true;
+}
+
 }
 
