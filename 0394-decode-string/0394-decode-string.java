876ee449d8056ef31c34ed2a6a404b29e50f0247
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2025-09-03 05:46:01 +0530

    Time: 0 ms (100%), Space: 41.3 MB (96.87%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,27 +1,29 @@
 class Solution {
-    public String decodeString(String s) {
-    return dfs(s.toCharArray(), new int[]{0});
-}
-
-private String dfs(char[] arr, int[] idx) {
-    StringBuilder curr = new StringBuilder();
+public String decodeString(String s) {
+    Stack<Integer> countStack = new Stack<>();   // stores repeat counts
+    Stack<StringBuilder> strStack = new Stack<>(); // stores previous strings
+    StringBuilder curr = new StringBuilder();   // current substring
     int num = 0;
 
-    while (idx[0] < arr.length) {
-        char ch = arr[idx[0]];
+    for (char ch : s.toCharArray()) {
         if (Character.isDigit(ch)) {
-            num = num * 10 + (ch - '0');
-        } else if (ch == '[') {
-            idx[0]++;
-            String sub = dfs(arr, idx);
-            curr.append(sub.repeat(num));
-            num = 0;
-        } else if (ch == ']') {
-            return curr.toString();
-        } else {
-            curr.append(ch);
+            num = num * 10 + (ch - '0'); // build full number (like "12")
+        } 
+        else if (ch == '[') {
+            countStack.push(num);      // save number
+            strStack.push(curr);       // save current string
+            curr = new StringBuilder();// reset for new substring
+            num = 0;                   // reset number
+        } 
+        else if (ch == ']') {
+            int repeat = countStack.pop(); // get repeat count
+            StringBuilder prev = strStack.pop(); // get prev string
+            // append repeated substring to previous string
+            curr = prev.append(curr.toString().repeat(repeat));
+        } 
+        else {
+            curr.append(ch); // normal character
         }
-        idx[0]++;
     }
     return curr.toString();
 }
