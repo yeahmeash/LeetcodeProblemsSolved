a9c10bae4fe3c33ce3d0db2912c17f57c4bf801c
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2025-09-05 13:46:02 +0530

    Time: 23 ms (14.11%), Space: 50.3 MB (41.23%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
@@ -1,17 +1,21 @@
 class KthLargest {
     int k;
-    List<Integer> nums;
+  PriorityQueue<Integer> pq=new PriorityQueue<>();
 
     public KthLargest(int k, int[] nums) {
         this.k = k;
-        this.nums = new ArrayList<>();
-        for (int num : nums) this.nums.add(num);
+         pq = new PriorityQueue<>();
+         for(int num:nums){
+            pq.add(num);
+            if(pq.size()>k) pq.poll();
+         }
+
     }
 
     public int add(int val) {
-        nums.add(val);
-        Collections.sort(nums, Collections.reverseOrder());
-        return nums.get(k - 1);
+        pq.add(val);
+         if(pq.size()>k) pq.poll();
+        return pq.peek();
     }
 }
 
