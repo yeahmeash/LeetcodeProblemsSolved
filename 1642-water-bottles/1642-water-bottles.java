9b35fbe51cf215fb4aed1a3097c9fb9a54bf21f8
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2025-10-02 03:58:48 +0530

    Time: 0 ms (100%), Space: 40.7 MB (20.1%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,8 +1,15 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-     
-    return numBottles+(numBottles-1)/(numExchange -1);
-
-
+        int maxBottles = 0;
+        int empties = 0;
+        
+        while (numBottles > 0) {
+            maxBottles += numBottles;      // drink them
+            empties += numBottles;         // now you have these empties
+            numBottles = empties / numExchange;  // exchange for new full bottles
+            empties = empties % numExchange;     // leftovers remain
+        }
+        
+        return maxBottles;
     }
 }
